61d16b731b93fab021e61bb165d36820ddda97b3
felixity1917 <77635620+felixity1917@users.noreply.github.com>
2024-11-10 20:55:10 +0530

    Add:control-structure.c

---
+++ clang-practice/control-structures.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int integer_input;
+
+  while (1) {
+    printf("Enter an integer: ");
+    if (scanf("%d", &integer_input) == 1) break;
+    else {
+      printf("Invalid input. Please enter a valid integer.\n");
+      while (getchar() != '\n');
+    }
+  if (integer_input % 2 == 0) printf("The Integer is Even.");
+  else printf("The Integer is Odd.")
+  }
